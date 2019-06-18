488bf3ff3a833df85175cd85527458fe00efb7f2
杨玉卿 <897337158@qq.com>
2019-06-18 17:03:14 +0800

    进一步精确按钮消失的时机，将按键判定与超时判定区分开来

--- Assets/Scripts/MonkeyMan/hint.cs
+++ Assets/Scripts/MonkeyMan/hint.cs
@@ -8,6 +8,7 @@ public class hint : MonoBehaviour
 {
     private bool isProcessing = false;
     private string Key;
+    private float timeLast = 10000f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,22 +19,30 @@ public class hint : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        timeLast -= Time.deltaTime;
         if (Input.GetButtonDown(Key) && isProcessing)//按下对应按键且蓝圈在显示过程中则结束判定
         {
-            complete();
+            complete(true);
         }
         if (isProcessing)//若正在显示过程中则蓝圈随时间逐渐缩小
         {
             transform.Find("process").localScale -= new Vector3(1.2f * Time.deltaTime, 1.2f * Time.deltaTime, 1.2f * Time.deltaTime);
         }
-        if (transform.Find("process").localScale.x < 0.4f)//蓝圈缩小到一定程度则自动结束判定
+        if (transform.Find("process").localScale.x < 0.4f)//蓝圈缩小到一定程度则自动消失
         {
-            complete();
+            transform.Find("process").GetComponent<CanvasGroup>().alpha = 0;
+            transform.Find("process").GetComponent<CanvasGroup>().interactable = false;
+            transform.Find("process").GetComponent<CanvasGroup>().blocksRaycasts = false;
+        }
+        if (timeLast < (-0.1f))//超过最晚完美判定点则自动结束判定
+        {
+            complete(false);
         }
     }
 
     public void begin()//开始出现蓝圈
     {
+        timeLast = 0.5f;
         isProcessing = true;
         transform.Find("process").GetComponent<CanvasGroup>().alpha = 1;
         transform.Find("process").GetComponent<CanvasGroup>().interactable = true;
@@ -43,14 +52,17 @@ public class hint : MonoBehaviour
         transform.localScale = new Vector3(1f, 1f, 1f);
     }
 
-    public void complete()//结束判定，隐藏蓝圈
+    public void complete(bool isKeyDown)//结束判定，隐藏蓝圈，参数为消失是否为按键触发
     {
         isProcessing = false;
         transform.Find("process").GetComponent<CanvasGroup>().alpha = 0;
         transform.Find("process").GetComponent<CanvasGroup>().interactable = false;
         transform.Find("process").GetComponent<CanvasGroup>().blocksRaycasts = false;
         transform.Find("process").localScale = new Vector3(1f, 1f, 1f);
-        transform.DOScale(new Vector3(2f, 2f, 2f), 0.5f);
+        if (isKeyDown)
+        {
+            transform.DOScale(new Vector3(2f, 2f, 2f), 0.5f);
+        }
         Tweener tweener = GetComponent<CanvasGroup>().DOFade(0, 0.4f);
         tweener.onComplete = drop;
     }
