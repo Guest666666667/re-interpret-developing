8894cce7451af8baaefad7d869385f0863081381
杨玉卿 <897337158@qq.com>
2019-06-18 10:32:35 +0800

    添加提示圈的显示消失机制

--- Assets/Scripts/MonkeyMan/hint.cs
+++ Assets/Scripts/MonkeyMan/hint.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class hint : MonoBehaviour
 {
@@ -36,6 +37,8 @@ public class hint : MonoBehaviour
         transform.Find("process").GetComponent<CanvasGroup>().interactable = true;
         transform.Find("process").GetComponent<CanvasGroup>().blocksRaycasts = true;
         transform.Find("process").localScale = new Vector3(1f,1f,1f);
+        GetComponent<CanvasGroup>().DOFade(1, 0.2f);
+        transform.localScale = new Vector3(1f, 1f, 1f);
     }
 
     public void complete()//结束判定，隐藏蓝圈
@@ -45,5 +48,7 @@ public class hint : MonoBehaviour
         transform.Find("process").GetComponent<CanvasGroup>().interactable = false;
         transform.Find("process").GetComponent<CanvasGroup>().blocksRaycasts = false;
         transform.Find("process").localScale = new Vector3(1f, 1f, 1f);
+        transform.DOScale(new Vector3(2f, 2f, 2f), 0.5f);
+        GetComponent<CanvasGroup>().DOFade(0, 0.4f);
     }
 }
