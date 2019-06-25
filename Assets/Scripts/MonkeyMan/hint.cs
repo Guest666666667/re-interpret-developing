da769449398efb712eb2d75fe7e71555050bcb24
杨玉卿 <897337158@qq.com>
2019-06-25 15:23:47 +0800

    添加按钮模糊渐变清晰特效

--- Assets/Scripts/MonkeyMan/hint.cs
+++ Assets/Scripts/MonkeyMan/hint.cs
@@ -48,7 +48,10 @@ public class hint : MonoBehaviour
         transform.Find("process").GetComponent<CanvasGroup>().interactable = true;
         transform.Find("process").GetComponent<CanvasGroup>().blocksRaycasts = true;
         transform.Find("process").localScale = new Vector3(1f,1f,1f);
-        GetComponent<CanvasGroup>().DOFade(1, 0.2f);
+        //GetComponent<CanvasGroup>().DOFade(1, 0.2f);
+        DOTween.To(() => GetComponentInChildren<Image>().material.GetFloat("_AlphaScale"), x => GetComponentInChildren<Image>().material.SetFloat("_AlphaScale", x), 1f, 0.2f);
+        GetComponentInChildren<Image>().material.SetFloat("_Offset", 0.05f);
+        DOTween.To(() => GetComponentInChildren<Image>().material.GetFloat("_Offset"), x => GetComponentInChildren<Image>().material.SetFloat("_Offset",x), 0f, 0.5f);
         transform.localScale = new Vector3(1f, 1f, 1f);
     }
 
@@ -59,12 +62,14 @@ public class hint : MonoBehaviour
         transform.Find("process").GetComponent<CanvasGroup>().interactable = false;
         transform.Find("process").GetComponent<CanvasGroup>().blocksRaycasts = false;
         transform.Find("process").localScale = new Vector3(1f, 1f, 1f);
-        if (isKeyDown)
+        /*if (isKeyDown)
         {
             transform.DOScale(new Vector3(2f, 2f, 2f), 0.5f);
-        }
+        }*/
+        //Tweener tweener = GetComponent<CanvasGroup>().DOFade(0, 0.4f);
+        DOTween.To(() => GetComponentInChildren<Image>().material.GetFloat("_Offset"), x => GetComponentInChildren<Image>().material.SetFloat("_Offset", x), 0.05f, 0.4f);
+        Tweener tweener = DOTween.To(() => GetComponentInChildren<Image>().material.GetFloat("_AlphaScale"), x => GetComponentInChildren<Image>().material.SetFloat("_AlphaScale", x), 0f, 0.4f);
         transform.Find("keyHint/Text").GetComponent<Text>().text = (isPerfect ? "善" : "负");
-        Tweener tweener = GetComponent<CanvasGroup>().DOFade(0, 0.4f);
         tweener.onComplete = drop;
     }
     public void drop()
@@ -77,11 +82,11 @@ public class hint : MonoBehaviour
         transform.Find("keyHint/Text").GetComponent<Text>().text = Key.ToUpper();
         if (Key == "a" || Key == "d")
         {
-            transform.position -= new Vector3(0f, 165f, 0f);
+            transform.localPosition -= new Vector3(0f, 165f, 0f);
         }
         if (Key == "e" || Key == "d")
         {
-            transform.position += new Vector3(540f, 0f, 0f);
+            transform.localPosition += new Vector3(540f, 0f, 0f);
         }
         begin();
     }
