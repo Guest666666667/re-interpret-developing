933f9db96e80db9a19e2bf82abec13dbe7f4f40b
Administrator <Administrator@hg38>
2019-07-01 09:16:51 +0800

    UI更改

--- Assets/Scripts/panel/init.cs
+++ Assets/Scripts/panel/init.cs
@@ -7,12 +7,23 @@ public class init : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        UIManager.Instance.PushPanel(UIPanelType.fightSelectPart);
+        if (gameObject.scene.name == "mainScene")
+        {
+            UIManager.Instance.PushPanel(UIPanelType.begin);
+        }
+        else if (gameObject.scene.name == "fightGame")
+        {
+            UIManager.Instance.PushPanel(UIPanelType.fightSelectPart);
+        }
+        else if (gameObject.scene.name == "storyGame")
+        {
+            UIManager.Instance.PushPanel(UIPanelType.storyMain);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
