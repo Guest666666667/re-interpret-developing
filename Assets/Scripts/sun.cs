4b80b98cfd0d83f705ff0f436edfd6f1d71dddf2
杨玉卿 <897337158@qq.com>
2019-07-11 20:14:22 +0800

    最终版本

---
+++ Assets/Scripts/sun.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sun : MonoBehaviour
+{
+    public float speed = 5f;
+    public float totalTime = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        totalTime = GameObject.Find("AudioManager").GetComponent<AudioManager>().MusicPlayer.time;
+        Vector2 v = transform.localPosition;
+        //if((totalTime>0f)&&(totalTime<6f))
+        //{
+        //    if(v.x<9.5f)
+        //    {
+        //        v.x += speed * Time.deltaTime;//向左移动
+        //    }
+           
+        //}
+        if((totalTime > 116f) && (totalTime < 126f))
+        {
+            v.x += 10 * Time.deltaTime*10;//向左移动
+        }
+      
+        transform.localPosition = v;
+    }
+}
---
+++ Assets/Scripts/upTake.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class upTake : MonoBehaviour
+{
+    private float totalTime;
+    private bool hasUp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hasUp = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        totalTime = GameObject.Find("AudioManager").GetComponent<AudioManager>().MusicPlayer.time;
+        if (!hasUp && totalTime >= 50f)
+        {
+            hasUp = true;
+            Sequence s = DOTween.Sequence();
+            s.Append(transform.DOMoveY(-3.13f, 1.5f))
+                .Append(DOTween.To(() => GetComponent<SpriteRenderer>().color.a, x => GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, x), 1f, 1f))
+                .Append(DOTween.To(() => GetComponent<SpriteRenderer>().color.a, x => GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, x), 0f, 1f))
+                .Append(DOTween.To(() => GetComponent<SpriteRenderer>().color.a, x => GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, x), 1f, 1f))
+                .Append(DOTween.To(() => GetComponent<SpriteRenderer>().color.a, x => GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, x), 0f, 1f));
+        }
+    }
+}
